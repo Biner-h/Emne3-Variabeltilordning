297cc5bc27b1e1c91f0d3ae856ada77f64bb1517
Biner Habib <Binerhabib@gmail.com>
2023-11-14 14:26:22 +0100

    Add project files.

---
+++ Emne3-Variabeltilordning/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Emne3_Variabeltilordning
+{
+    internal class Program
+    {   //Modell
+        int tall1 = 5;
+        int tall2 = 15;
+
+        string tekst = "Denne returnerer ingenting";
+
+        public int getANumber(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        //View
+        public void run()
+        { 
+            Console.WriteLine($"Svaret er: {getANumber(tall1, tall2)}");
+        }
+        public void nothing()
+        {
+            Console.WriteLine(tekst);
+        }
+
+        //Controller
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.run();
+            program.nothing();
+        }
+    }
+}
